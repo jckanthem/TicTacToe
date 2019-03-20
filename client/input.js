7b6bf52977ce7d0ec17b320f06c6a4f5b1aa2c6a
James Kelly <jamesckelly99@gmail.com>
2019-03-19 21:18:44 -0700

    refactored and updated view to be more efficient in DOM accessing

--- client/input.js
+++ client/input.js
@@ -6,7 +6,7 @@ class Input {
   onClick(context, state, view){
     if(!['X', 'O'].includes(context.innerHTML)){
       view.changeTurn(context,state, this.player1, this.player2);
-      let coords = context.className.slice(-2)
+      let coords = context.id.slice(-2);
       state.updateBoard(coords[1],coords[0]);
       if(state.checkWin(coords[1],coords[0])){
         let winner = state.turn ? 'X' : 'O';
--- client/view.js
+++ client/view.js
@@ -1,35 +1,52 @@
 class View {
+  constructor(){
+    this.player1 = document.getElementById('player1');
+    this.player2 = document.getElementById('player2');
+    this.restartButton = document.querySelector('button');
+    this.turnSpan = document.getElementById('turn');
+    this.winMessage = document.getElementById('winMessage');
+    this.boxes = document.querySelectorAll('.board > div');
+    this.gameCount = document.getElementById('gameCount');
+    this.ties = document.getElementById('ties');
+    this.xWins = document.getElementById('xWins');
+    this.oWins = document.getElementById('oWins');
+    this.updateScoreboard = this.updateScoreboard.bind(this);
+    this.restartView = this.restartView.bind(this);
+    this.changeTurn = this.changeTurn.bind(this);
+    this.tieGameUpdate = this.tieGameUpdate.bind(this);
+    this.endGameViewUpdate = this.endGameViewUpdate.bind(this);
+  }
   updateScoreboard(input, state){
-    document.getElementById('player1').innerHTML = `${(input.player1)} (X)`;
-    document.getElementById('player2').innerHTML = `${input.player2} (O)`;
+    this.player1.innerHTML = `${(input.player1)} (X)`;
+    this.player2.innerHTML = `${input.player2} (O)`;
   }
   restartView(){
-    document.querySelector('button').style.display = "none";
-    document.getElementById('turn').style.display = "";
-    document.getElementById('winMessage').style.display = "none";
-    document.querySelectorAll('.board > div').forEach(box => {
+    this.restartButton.style.display = "none";
+    this.turnSpan.style.display = "";
+    this.winMessage.style.display = "none";
+    this.boxes.forEach(box => {
       box.innerHTML = '';
     });
   }
   changeTurn(context, state, player1, player2){
     context.innerHTML = state.turn ? 'X' : 'O';
-    document.getElementById('turn').innerHTML = `<b>Turn:</b> ${state.turn ? player1 + ' (X)' : player2 + ' (O)'}`;
+    this.turnSpan.innerHTML = `<b>Turn:</b> ${!state.turn ? player1 + ' (X)' : player2 + ' (O)'}`;
   }
   tieGameUpdate(){
-    document.getElementById('gameCount').innerHTML = state.score.player1 + state.score.player2 + state.score.ties;
-    document.getElementById('ties').innerHTML = state.score.ties;
-    document.getElementById('winMessage').innerHTML = `Tie Game!`
-    document.getElementById('winMessage').style.display = "";
-    document.querySelector('button').style.display = "";
-    document.getElementById('turn').style.display = "none";
+    this.gameCount.innerHTML = state.score.player1 + state.score.player2 + state.score.ties;
+    this.ties.innerHTML = state.score.ties;
+    this.winMessage.innerHTML = `Tie Game!`
+    this.winMessage.style.display = "";
+    this.restartButton.style.display = "";
+    this.turnSpan.style.display = "none";
   }
   endGameViewUpdate(winner, state, player1, player2){
-    document.getElementById('gameCount').innerHTML = state.score.player1 + state.score.player2 + state.score.ties;
-    document.getElementById('xWins').innerHTML = state.score.player1;
-    document.getElementById('oWins').innerHTML = state.score.player2;
-    document.getElementById('winMessage').innerHTML = `${winner === 'X' ? player1 + ' (X)' : player2 + ' (O)'} Wins!`;
-    document.getElementById('winMessage').style.display = "";
-    document.querySelector('button').style.display = "";
-    document.getElementById('turn').style.display = "none";
+    this.gameCount.innerHTML = state.score.player1 + state.score.player2 + state.score.ties;
+    this.xWins.innerHTML = state.score.player1;
+    this.oWins.innerHTML = state.score.player2;
+    this.winMessage.innerHTML = `${winner === 'X' ? player1 + ' (X)' : player2 + ' (O)'} Wins!`;
+    this.winMessage.style.display = "";
+    this.restartButton.style.display = "";
+    this.turnSpan.style.display = "none";
   }
 }
